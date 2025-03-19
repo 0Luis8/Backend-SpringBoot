cef3a005e47d8a11d4f613eabac9cd4df0c3705a
Luis de Castro Mora <luisdecmora@gmail.com>
2025-03-19 10:55:36 +0100

    Primer commit - Añadiendo proyecto SpringBoot creado con NetBeans

---
+++ peliculas/src/test/java/com/_Luis8/peliculas/PeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Luis8.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
